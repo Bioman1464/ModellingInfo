6a2b864595b1ec86e33849b666d9337b9547756c
Bioman1464 <danilgap1464@gmail.com>
2021-04-10 09:16:44 +0300

    feat(main activity): setup host navigation

--- app/src/main/java/bio/models/three_d/MainMenuActivity.kt
+++ app/src/main/java/bio/models/three_d/MainMenuActivity.kt
@@ -2,10 +2,19 @@ package bio.models.three_d
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.main_nav_graph) as NavHostFragment? ?: return
+
+        val navController = host.navController
+
     }
+
 }
