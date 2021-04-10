e39ad836f99f1f66543c0d2da39c932d23d1d3bb
Bioman1464 <danilgap1464@gmail.com>
2021-04-10 09:15:42 +0300

    feat: setup bottom navigation & nested navigation

--- app/src/main/java/bio/models/three_d/MainMenuNavFragment.kt
+++ app/src/main/java/bio/models/three_d/MainMenuNavFragment.kt
@@ -1,21 +1,43 @@
 package bio.models.three_d
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainMenuNavFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var nestedNavController: NavController
+    private lateinit var mainNavController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_main_menu_nav, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_menu_nav, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainNavController = Navigation.findNavController(requireActivity(), R.id.main_nav_host_fragment)
+
+        val nestedNavHostFragment = childFragmentManager
+            .findFragmentById(R.id.menu_nav_host_fragment) as? NavHostFragment
+        nestedNavHostFragment?.navController.let {
+            nestedNavController = it!!
+        }
+        setupBottomNavMenu(view, nestedNavController)
     }
+
+    private fun setupBottomNavMenu(view: View, navController: NavController) {
+        val bottomNav = view.findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+        bottomNav?.run {
+            setupWithNavController(navController)
+            itemIconTintList = null
+        }
+    }
+
 }
