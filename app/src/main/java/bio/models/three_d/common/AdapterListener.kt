5e29c05934c60eb10a0aed1c93493507bf79b954
Bioman1464 <danilgap1464@gmail.com>
2021-04-14 09:43:34 +0300

    feat(common): add adapter listener

--- app/src/main/java/bio/models/three_d/common/AdapterListener.kt
+++ app/src/main/java/bio/models/three_d/common/AdapterListener.kt
@@ -1,6 +1,6 @@
 package bio.models.three_d.common
 
 interface AdapterListener {
-    fun listen(click: AdapterClick?)
+    fun listen(click: AdapterClick?, position: Int)
 }
 interface AdapterClick
