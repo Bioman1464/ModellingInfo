a4f7c0548d981dd3ead8c40e7fa1b5027440c0b4
Bioman1464 <danilgap1464@gmail.com>
2021-04-14 09:52:08 +0300

    feat(page.search): add recycler

--- app/src/main/java/bio/models/three_d/main_menu/search/SearchFragment.kt
+++ app/src/main/java/bio/models/three_d/main_menu/search/SearchFragment.kt
@@ -1,22 +1,52 @@
 package bio.models.three_d.main_menu.search
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import bio.models.three_d.R
+import bio.models.three_d.common.AdapterClick
+import bio.models.three_d.common.AdapterListener
+import bio.models.three_d.databinding.FragmentHomeBinding
+import bio.models.three_d.databinding.FragmentSearchBinding
+import bio.models.three_d.main_menu.common.MainAdapter
+import bio.models.three_d.main_menu.common.MarginItemDecoration
+import bio.models.three_d.main_menu.home.theme.Theme
+import bio.models.three_d.main_menu.home.theme.ThemeData
 
-class SearchFragment : Fragment() {
+class SearchFragment : Fragment(R.layout.fragment_search), AdapterListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private lateinit var binding : FragmentSearchBinding
+    private val listAdapter by lazy { MainAdapter(this) }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSearchBinding.bind(view)
+        initialize()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    private fun initialize() {
+        binding.searchRecycler.apply {
+            addItemDecoration(
+                MarginItemDecoration(
+                resources
+                    .getDimension(R.dimen.recycler_item_spacing)
+                    .toInt()
+            ))
+            layoutManager = LinearLayoutManager(context)
+            adapter = listAdapter
+        }
+
+        listAdapter.submitList(ThemeData.createList())
     }
+
+    override fun listen(click: AdapterClick?, position: Int) {
+        if (click is Theme) {
+            Log.d("TEST", "Item is clicked: ${click.theme}")
+        }
+    }
+
 }
