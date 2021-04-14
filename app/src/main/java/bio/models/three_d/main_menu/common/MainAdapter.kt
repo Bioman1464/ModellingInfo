ce283cac64f0ae5881e73640826569d2e0379bf4
Bioman1464 <danilgap1464@gmail.com>
2021-04-14 09:49:57 +0300

    feat(common): add article cell for item types

--- app/src/main/java/bio/models/three_d/main_menu/common/MainAdapter.kt
+++ app/src/main/java/bio/models/three_d/main_menu/common/MainAdapter.kt
@@ -2,9 +2,10 @@ package bio.models.three_d.main_menu.common
 
 import bio.models.three_d.common.AdapterListener
 import bio.models.three_d.common.BaseListAdapter
+import bio.models.three_d.main_menu.common.article.ArticleCell
 import bio.models.three_d.main_menu.home.theme.ThemeCell
 
 class MainAdapter(listener: AdapterListener) : BaseListAdapter(
-    ThemeCell,
+    ThemeCell, ArticleCell,
     listener = listener
 )
