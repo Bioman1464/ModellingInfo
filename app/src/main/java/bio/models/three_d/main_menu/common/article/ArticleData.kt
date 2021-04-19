547fc85c0977e4da52cb788f522391aecc767f46
Bioman1464 <danilgap1464@gmail.com>
2021-04-19 11:15:31 +0300

    feat(articleData): change factory

--- app/src/main/java/bio/models/three_d/main_menu/common/article/ArticleData.kt
+++ app/src/main/java/bio/models/three_d/main_menu/common/article/ArticleData.kt
@@ -5,48 +5,40 @@ import bio.models.three_d.common.RecyclerItem
 
 object ArticleData {
 
+    fun getTitle(index: Int): String {
+        return when(index) {
+            in 0..5 -> "Low-poly"
+            in 5..10 -> "Topology"
+            in 10..15 -> "Uv-развертка"
+            in 15..20 -> "High-poly"
+            in 20..25 -> "Bake"
+            else -> "Low-poly"
+        }
+    }
+
+    fun getThemeId(index: Int): Int {
+        return when(index) {
+            in 0..5 -> 0
+            in 5..10 -> 1
+            in 10..15 -> 2
+            in 15..20 -> 3
+            in 20..25 -> 4
+            else -> 1
+        }
+    }
+
     fun createList(): List<RecyclerItem> {
         val list = ArrayList<RecyclerItem>()
-        list.add(
-            Article(
-                id = "1",
-                title = "Low-poly",
-                imageSrc = R.drawable.item_image
+        for (i in 0..25) {
+            list.add(
+                Article(
+                    id = "$i",
+                    themeId = getThemeId(i),
+                    title = "${getTitle(i)}${i}",
+                    imageSrc = R.drawable.item_image
+                )
             )
-        )
-
-        list.add(
-            Article(
-                id = "2",
-                title = "Topology",
-                imageSrc = R.drawable.item_image
-            )
-        )
-
-        list.add(
-            Article(
-                id = "3",
-                title = "UV-развертка",
-                imageSrc = R.drawable.item_image
-            )
-        )
-
-        list.add(
-            Article(
-                id = "4",
-                title = "High-poly",
-                imageSrc = R.drawable.item_image
-            )
-        )
-
-        list.add(
-            Article(
-                id = "5",
-                title = "Bake",
-                imageSrc = R.drawable.item_image
-            )
-        )
-
+        }
         return list
     }
 }
