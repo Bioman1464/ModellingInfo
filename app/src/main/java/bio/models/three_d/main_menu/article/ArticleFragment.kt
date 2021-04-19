987f48ff54c1e1c5a032064c3ac1d54b7518e9e4
Bioman1464 <danilgap1464@gmail.com>
2021-04-20 01:18:59 +0300

    feat(page.article): add change favorite fun

--- app/src/main/java/bio/models/three_d/main_menu/article/ArticleFragment.kt
+++ app/src/main/java/bio/models/three_d/main_menu/article/ArticleFragment.kt
@@ -2,39 +2,80 @@ package bio.models.three_d.main_menu.article
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import bio.models.three_d.R
+import bio.models.three_d.common.Article
+import bio.models.three_d.common.SharedPrefs
 import bio.models.three_d.databinding.FragmentArticleBinding
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
     private lateinit var navArgs: ArticleFragmentArgs
     private lateinit var binding: FragmentArticleBinding
-
+    private lateinit var prefs: SharedPrefs
+    private lateinit var article: Article
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navArgs = ArticleFragmentArgs.fromBundle(requireArguments())
         binding = FragmentArticleBinding.bind(view)
+        prefs = SharedPrefs.getInstance(requireContext())
         setTitle()
-        showArticleFromHtml("myfile.html")
+        initView()
+    }
+
+    private fun initView() {
+        binding.addToFavourite.setOnClickListener {
+            changeFavourite()
+        }
     }
 
     private fun setTitle() {
         val articleId = navArgs.articleId
+        article = Article(articleId, 0)
         val themeList = resources.getStringArray(R.array.tech_article_titles)
         articleId.let {
-            binding.articleTitle.text = themeList[it]
-            getArticleById(it)
+            binding.articleTitle.text = themeList[it%6]
+            checkIfFavourite()
+            getArticleById()
         }
     }
 
-    private fun changeFavourite() {
+    private fun checkIfFavourite() {
+        val isFavourite = prefs.checkArticleFavourite(article)
+        Log.d("checkIfFavourite", isFavourite.toString())
+        setFavouriteStar(isFavourite)
     }
 
-    private fun getArticleById(id: Int) {
+    private fun setFavouriteStar(isFavourite: Boolean) {
+        if (isFavourite) {
+            binding.addToFavourite.setImageResource(R.drawable.ic_star_blue)
+            return
+        }
+        binding.addToFavourite.setImageResource(R.drawable.ic_star_blue_outline)
+    }
 
+    private fun changeFavourite() {
+        val prefsResponse: Boolean?
+        if (prefs.checkArticleFavourite(article)) {
+            setFavouriteStar(false)
+            prefsResponse = prefs.unfavouriteArticle(article)
+        } else {
+            setFavouriteStar(true)
+            prefsResponse = prefs.setFavouriteArticle(article)
+        }
+        if (prefsResponse == null) {
+            Toast.makeText(requireContext(), "Ошибка записи в базу данных", Toast.LENGTH_SHORT)
+                .show()
+            setFavouriteStar(prefs.checkArticleFavourite(article))
+        }
+    }
+
+    private fun getArticleById() {
+        showArticleFromHtml("myfile.html")
     }
 
     private fun showArticleFromHtml(fileName: String) {
