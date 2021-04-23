cdd09d5295df4db6e84947a0c53be0f9f270472c
Bioman1464 <danilgap1464@gmail.com>
2021-04-23 21:56:49 +0300

    feat(common): add search article type to adapter

--- app/src/main/java/bio/models/three_d/main_menu/common/MainAdapter.kt
+++ app/src/main/java/bio/models/three_d/main_menu/common/MainAdapter.kt
@@ -4,8 +4,9 @@ import bio.models.three_d.common.AdapterListener
 import bio.models.three_d.common.BaseListAdapter
 import bio.models.three_d.main_menu.common.article.ArticleCell
 import bio.models.three_d.main_menu.home.theme.ThemeCell
+import bio.models.three_d.main_menu.search.search_recycler.SearchArticleCell
 
 class MainAdapter(listener: AdapterListener) : BaseListAdapter(
-    ThemeCell, ArticleCell,
+    ThemeCell, ArticleCell, SearchArticleCell,
     listener = listener
 )
