6ca2a60a0d86988bc955c7dddbf8696dfe9c9931
Bioman1464 <danilgap1464@gmail.com>
2021-05-01 19:43:46 +0300

    feat(page.settings): change theme on radio click & save to shared prefs

--- app/src/main/java/bio/models/three_d/main_menu/settings/SettingFragment.kt
+++ app/src/main/java/bio/models/three_d/main_menu/settings/SettingFragment.kt
@@ -1,12 +1,11 @@
 package bio.models.three_d.main_menu.settings
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
 import bio.models.three_d.R
+import bio.models.three_d.common.ThemeSharedPrefs
 import bio.models.three_d.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment(R.layout.fragment_setting){
@@ -17,5 +16,25 @@ class SettingFragment : Fragment(R.layout.fragment_setting){
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSettingBinding.bind(view)
 
+        val themeId = ThemeSharedPrefs.getInstance(requireContext())
+        when (themeId.getTheme()) {
+            0 -> { binding.radioDayTheme.isChecked = true }
+            1 -> { binding.radioNightTheme.isChecked = true }
+        }
+
+        binding.colorThemeRadioGroup.setOnCheckedChangeListener { _, id ->
+            when (id) {
+                binding.radioDayTheme.id -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    themeId.setTheme(0)
+                    return@setOnCheckedChangeListener
+                }
+                binding.radioNightTheme.id -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    themeId.setTheme(1)
+                    return@setOnCheckedChangeListener
+                }
+            }
+        }
     }
 }
