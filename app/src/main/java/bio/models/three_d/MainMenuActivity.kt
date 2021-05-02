1011d922d49c4233e57168fa467731de4313cd6a
Bioman1464 <danilgap1464@gmail.com>
2021-05-02 16:34:01 +0300

    feat: add user autoLogIn on app start

--- app/src/main/java/bio/models/three_d/MainMenuActivity.kt
+++ app/src/main/java/bio/models/three_d/MainMenuActivity.kt
@@ -1,9 +1,16 @@
 package bio.models.three_d
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import bio.models.three_d.common.UserAccount
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainMenuActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
@@ -11,5 +18,27 @@ class MainMenuActivity : AppCompatActivity() {
 //        val host: NavHostFragment = supportFragmentManager
 //            .findFragmentById(R.id.main_nav_graph) as NavHostFragment? ?: return
 //        val navController = host.navController
+        auth = FirebaseAuth.getInstance()
+        getFirebaseUser()
+    }
+
+    private fun getFirebaseUser() {
+        auth.let {
+            val user = auth.currentUser
+            user?.let {
+                updateAccountUiInfo(user.uid, user.photoUrl, user.displayName)
+            }
+        }
+    }
+
+    private fun updateAccountUiInfo(userUid: String?, userPhotoUrl: Uri?, userName: String?) {
+        UserAccount.run {
+            if (userUid != null) {
+                uid = userUid
+            }
+            name = userName
+            photoUrl = userPhotoUrl
+            logData()
+        }
     }
 }
