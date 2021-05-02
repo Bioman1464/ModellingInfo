44dc56a5814893aab0a342ba56d9e1ad5ed0d2bd
Bioman1464 <danilgap1464@gmail.com>
2021-05-02 16:35:16 +0300

    feat(page.settings): add google & firebase logIn on user container click

--- app/src/main/java/bio/models/three_d/main_menu/settings/SettingFragment.kt
+++ app/src/main/java/bio/models/three_d/main_menu/settings/SettingFragment.kt
@@ -1,40 +1,194 @@
 package bio.models.three_d.main_menu.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import bio.models.three_d.R
+import bio.models.three_d.common.ImageDownloadTask
 import bio.models.three_d.common.ThemeSharedPrefs
+import bio.models.three_d.common.UserAccount
 import bio.models.three_d.databinding.FragmentSettingBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
-class SettingFragment : Fragment(R.layout.fragment_setting){
 
+class SettingFragment : Fragment(R.layout.fragment_setting) {
+
+    private val TAG = this::class.java.simpleName
+    private lateinit var auth: FirebaseAuth
     private lateinit var binding: FragmentSettingBinding
+    private lateinit var themeSharedPrefs: ThemeSharedPrefs
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSettingBinding.bind(view)
+        themeSharedPrefs = ThemeSharedPrefs.getInstance(requireContext())
+        auth = FirebaseAuth.getInstance()
 
-        val themeId = ThemeSharedPrefs.getInstance(requireContext())
-        when (themeId.getTheme()) {
-            0 -> { binding.radioDayTheme.isChecked = true }
-            1 -> { binding.radioNightTheme.isChecked = true }
+        setSettingsConfiguration()
+
+        setClickListeners()
+
+        configureUserData()
+    }
+
+    private fun setSettingsConfiguration() {
+        when (themeSharedPrefs.getTheme()) {
+            0 -> {
+                binding.radioDayTheme.isChecked = true
+            }
+            1 -> {
+                binding.radioNightTheme.isChecked = true
+            }
         }
+    }
 
+    private fun setClickListeners() {
         binding.colorThemeRadioGroup.setOnCheckedChangeListener { _, id ->
             when (id) {
                 binding.radioDayTheme.id -> {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                    themeId.setTheme(0)
+                    themeSharedPrefs.setTheme(0)
                     return@setOnCheckedChangeListener
                 }
                 binding.radioNightTheme.id -> {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                    themeId.setTheme(1)
+                    themeSharedPrefs.setTheme(1)
                     return@setOnCheckedChangeListener
                 }
             }
         }
+
+        binding.profileLayout.setOnClickListener {
+            if (UserAccount.uid.isEmpty()) {
+                logInUser()
+                return@setOnClickListener
+            }
+            logOutUser()
+        }
     }
+
+    private fun configureUserData() {
+        if (UserAccount.uid.isEmpty()) {
+            return
+        }
+        Log.d(TAG, "User is empty")
+        updateUiAccountInfo(UserAccount.photoUrl, UserAccount.name)
+    }
+
+    private fun logInUser() {
+        if (checkIfLoggedIn()) {
+            updateUiAccountInfo(UserAccount.photoUrl, UserAccount.name)
+            return
+        }
+
+        val googleSignInOptions = getGoogleSignInOptions()
+        googleSignInOptions?.let {
+            val mGoogleSignInClient = GoogleSignIn.getClient(requireContext(), it)
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, 1)
+        }
+    }
+
+    private fun checkIfLoggedIn(): Boolean {
+        auth.currentUser ?: return false
+        return true
+    }
+
+    private fun getGoogleSignInOptions(): GoogleSignInOptions? {
+        return GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).run {
+            requestIdToken(getString(R.string.sign_in_key_id))
+            requestEmail()
+            requestId()
+            build()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+            account?.let {
+                updateUiAccountInfo(it.photoUrl, it.displayName)
+                firebaseAuthWithGoogle(it.idToken.toString())
+            }
+        } catch (e: ApiException) {
+            Log.d(TAG, "Error while google sign in: CODE: ${e.statusCode} MESSAGE: ${e.message}")
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        if (idToken.isEmpty()) {
+            return
+        }
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) {
+                if (it.isSuccessful) {
+                    val user = auth.currentUser
+                    user?.let {
+                        UserAccount.run {
+                            uid = it.uid
+                            name = it.displayName
+                            photoUrl = it.photoUrl
+                        }
+                    }
+
+                    Log.d(TAG, "Successful sign-in through firebase")
+                    return@addOnCompleteListener
+                }
+                Log.w(TAG, "signInWithCredential:failure", it.exception)
+            }
+    }
+
+    private fun logOutUser() {
+        auth.signOut()
+        UserAccount.clearData()
+        googleLogOut()
+        updateUiAccountInfo(null, null)
+    }
+
+    private fun googleLogOut() {
+        val googleSignOptions = getGoogleSignInOptions()
+        googleSignOptions?.let {
+            val googleClient = GoogleSignIn.getClient(requireActivity(), it)
+            googleClient.revokeAccess().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "Access successfully revoked")
+                    return@addOnCompleteListener
+                }
+                if (task.isCanceled) {
+                    Log.d(TAG, "Access revoke canceled")
+                    return@addOnCompleteListener
+                }
+            }
+        }
+    }
+
+    private fun updateUiAccountInfo(userImageUrl: Uri?, userName: String?) {
+        if (userImageUrl != null) {
+            ImageDownloadTask(binding.userImage).execute(userImageUrl.toString())
+        } else {
+            binding.userImage.setImageResource(R.drawable.ic_user_image)
+        }
+        binding.userNickname.text = userName ?: getString(R.string.nickname)
+    }
+
 }
