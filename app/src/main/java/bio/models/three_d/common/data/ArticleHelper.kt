90be003c4d93ff8b37543c72ac94ec69b67407ae
Bioman1464 <danilgap1464@gmail.com>
2021-05-03 22:21:17 +0300

    feat(helper.article): add getIdsFromArticles, parseArticlesFromRow methods

--- app/src/main/java/bio/models/three_d/common/data/ArticleHelper.kt
+++ app/src/main/java/bio/models/three_d/common/data/ArticleHelper.kt
@@ -1,6 +1,36 @@
 package bio.models.three_d.common.data
 
+import android.util.Log
+
 object ArticleHelper {
+
+    private val TAG = this::class.java.simpleName
+
+    fun getIdsListFromArticles(articles: List<Article>): ArrayList<Int> {
+        val articleIds = arrayListOf<Int>()
+        if (articles.isEmpty()) {
+            return articleIds
+        }
+        for (article in articles) {
+            articleIds.add(article.id)
+        }
+        return articleIds
+    }
+
+    fun parseRawArticleIds(rawData: String): List<Article> {
+        Log.d(TAG, "RawArticleIds: ${rawData}")
+        val ids = rawData.split(",").map { it.trim() }
+        val articleList: MutableList<Article> = mutableListOf()
+        try {
+            ids.forEach { id ->
+                articleList.add(Article(id.toInt(), themeIdById(id.toInt())))
+            }
+        } catch (e: NumberFormatException) {
+            Log.d(TAG, "Can't parse")
+        }
+        Log.d(TAG, "ParsedArticleIds: ${articleList.toString()}")
+        return articleList
+    }
 
     fun themeIdById(articleId: Int): Int {
         return when (articleId) {
