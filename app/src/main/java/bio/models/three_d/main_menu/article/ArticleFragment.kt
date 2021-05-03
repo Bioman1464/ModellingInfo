be3d4b374c1ccf9e5ade2ef4e8f3a257fbdce3e3
Bioman1464 <danilgap1464@gmail.com>
2021-05-03 22:23:20 +0300

    feat(page.article): add/remove favourite article on firebase/storage

--- app/src/main/java/bio/models/three_d/main_menu/article/ArticleFragment.kt
+++ app/src/main/java/bio/models/three_d/main_menu/article/ArticleFragment.kt
@@ -9,12 +9,18 @@ import androidx.fragment.app.Fragment
 import bio.models.three_d.R
 import bio.models.three_d.common.data.Article
 import bio.models.three_d.common.ArticleSharedPrefs
+import bio.models.three_d.common.UserAccount
+import bio.models.three_d.common.article.ArticleRepository
+import bio.models.three_d.common.data.ArticleHelper
+import bio.models.three_d.common.firebase.data.FirebaseDataHelper
 import bio.models.three_d.databinding.FragmentArticleBinding
 import bio.models.three_d.main_menu.common.article.ArticleData
 import bio.models.three_d.main_menu.home.theme.ThemeData
+import com.google.firebase.database.snapshot.BooleanNode
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
+    private val TAG = this::class.java.simpleName
     private lateinit var navArgs: ArticleFragmentArgs
     private lateinit var binding: FragmentArticleBinding
     private lateinit var prefsArticle: ArticleSharedPrefs
@@ -25,62 +31,116 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
         navArgs = ArticleFragmentArgs.fromBundle(requireArguments())
         binding = FragmentArticleBinding.bind(view)
         prefsArticle = ArticleSharedPrefs.getInstance(requireContext())
-        setTitle()
+        showArticleData()
         initView()
     }
 
     private fun initView() {
         binding.addToFavourite.setOnClickListener {
-            changeFavourite()
+            changeFavouriteState()
         }
     }
 
-    private fun setTitle() {
+    private fun showArticleData() {
         val articleId = navArgs.articleId
-        article = Article(articleId, 0)
         articleId.let {
+            showArticle()
             val article = ArticleData.getById(requireContext(), it)
             val theme = ThemeData.getById(requireContext(), article.themeId)
-
-            binding.articleTitle.text = article.title
-            binding.themeTitle.text = theme?.theme
-
+            this.article = Article(articleId, theme?.id?.toInt() ?: 0)
+            updateHeader(theme?.theme ?: "", article.title)
             checkIfFavourite()
-            getArticleById()
         }
+    }
+
+    private fun updateHeader(themeTitle: String, articleTitle: String) {
+        binding.articleTitle.text = articleTitle
+        binding.themeTitle.text = themeTitle
     }
 
     private fun checkIfFavourite() {
         val isFavourite = prefsArticle.checkArticleFavourite(article)
         Log.d("checkIfFavourite", isFavourite.toString())
-        setFavouriteStar(isFavourite)
+        changeArticleStartImage(isFavourite)
     }
 
-    private fun setFavouriteStar(isFavourite: Boolean) {
-        if (isFavourite) {
+    private fun changeArticleStartImage(filled: Boolean) {
+        if (filled) {
             binding.addToFavourite.setImageResource(R.drawable.ic_star_blue)
             return
         }
         binding.addToFavourite.setImageResource(R.drawable.ic_star_blue_outline)
     }
 
-    private fun changeFavourite() {
-        val prefsResponse: Boolean?
-        if (prefsArticle.checkArticleFavourite(article)) {
-            setFavouriteStar(false)
-            prefsResponse = prefsArticle.unfavouriteArticle(article)
+    private fun changeFavouriteState() {
+        val isFavourite: Boolean = prefsArticle.checkArticleFavourite(article)
+        changeArticleStartImage(!isFavourite)
+        if (UserAccount.uid.isEmpty()) {
+            Log.d(TAG, "Update shared prefs")
+            updateDatabaseData(isFavourite)
+            return
+        }
+        Log.d(TAG, "Checking if firebase updated")
+        updateLocalData(isFavourite)
+    }
+
+    private fun checkForUpdates(isFavourite: Boolean, articles: List<Article>): Boolean {
+        if (isFavourite) {
+            if (articles.contains(article)) {
+                return true
+            }
+            return false
+        }
+        if (articles.contains(article)) {
+            return false
+        }
+        return true
+    }
+
+    private fun updateLocalData(isFavourite: Boolean) {
+        val task = FirebaseDataHelper
+            .getUserFavouriteReference(requireContext(), UserAccount.uid)
+            .get()
+
+        task.addOnCompleteListener {
+           if (it.isSuccessful) {
+               val articles = ArticleHelper.parseRawArticleIds(it.result!!.value.toString())
+               val needUpdate = checkForUpdates(isFavourite, articles)
+               if (needUpdate) {
+                   Log.d(TAG, "Update ")
+                   updateDatabaseData(isFavourite)
+                   return@addOnCompleteListener
+               }
+               Log.d(TAG, "Update only shared prefs")
+               updateSharedPrefs(articles)
+           }
+        }
+    }
+
+    private fun updateSharedPrefs(articles: List<Article>) {
+        val sharedPreferences = ArticleSharedPrefs.getInstance(requireContext())
+        sharedPreferences.reloadFavouriteArticleList(articles)
+    }
+
+    private fun updateDatabaseData(isFavourite: Boolean) {
+        val articleRepository = ArticleRepository(
+            ArticleSharedPrefs.getInstance(requireContext()),
+            FirebaseDataHelper.getUserFavouriteReference(requireContext(), UserAccount.uid)
+        )
+        val prefsResponse: Boolean? = if (isFavourite) {
+            articleRepository.removeArticleFromFavourite(article)
         } else {
-            setFavouriteStar(true)
-            prefsResponse = prefsArticle.setFavouriteArticle(article)
+            articleRepository.addArticleToFavourite(article)
         }
         if (prefsResponse == null) {
             Toast.makeText(requireContext(), "Ошибка записи в базу данных", Toast.LENGTH_SHORT)
                 .show()
-            setFavouriteStar(prefsArticle.checkArticleFavourite(article))
+            changeArticleStartImage(prefsArticle.checkArticleFavourite(article))
         }
     }
 
-    private fun getArticleById() {
+    private fun showArticle() {
+        //TODO:: get article file by id
         showArticleFromHtml("myfile.html")
     }
 
