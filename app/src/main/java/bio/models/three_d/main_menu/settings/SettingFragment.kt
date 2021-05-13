98c8adaf3d36b8e4e3b5c8bbc829c6b210442fdb
Bioman1464 <danilgap1464@gmail.com>
2021-05-13 21:27:27 +0300

    feat(page.settings): get & set fontSize

--- app/src/main/java/bio/models/three_d/main_menu/settings/SettingFragment.kt
+++ app/src/main/java/bio/models/three_d/main_menu/settings/SettingFragment.kt
@@ -10,15 +10,13 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import bio.models.three_d.MainMenuActivity
 import bio.models.three_d.R
-import bio.models.three_d.common.shared_preferences.ArticleSharedPrefs
 import bio.models.three_d.common.ImageDownloadTask
-import bio.models.three_d.common.shared_preferences.ThemeSharedPrefs
 import bio.models.three_d.common.UserAccount
 import bio.models.three_d.common.firebase.data.FirebaseDataHelper
 import bio.models.three_d.common.firebase.user.FirebaseUserHelper
-import bio.models.three_d.common.shared_preferences.LanguageSharedPrefs
 import bio.models.three_d.databinding.FragmentSettingBinding
 import bio.models.three_d.Language
+import bio.models.three_d.common.shared_preferences.*
 import bio.models.three_d.main_menu.common.article.ArticleData
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -36,27 +34,42 @@ class SettingFragment : Fragment(R.layout.fragment_setting) {
     private lateinit var binding: FragmentSettingBinding
     private lateinit var themeSharedPrefs: ThemeSharedPrefs
     private lateinit var languageSharedPrefs: LanguageSharedPrefs
+    private lateinit var fontSizeSharedPrefs: FontSharedPrefs
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSettingBinding.bind(view)
         themeSharedPrefs = ThemeSharedPrefs.getInstance(requireContext())
         languageSharedPrefs = LanguageSharedPrefs.getInstance(requireContext())
+        fontSizeSharedPrefs = FontSharedPrefs.getInstance(requireContext())
         auth = FirebaseAuth.getInstance()
         setSettingsConfiguration()
-        setLanguageConfiguration()
         setClickListeners()
         configureUserData()
     }
 
     private fun setSettingsConfiguration() {
+        setFontSizeRadio()
+        setThemeRadio()
+        setLanguageRadio()
+    }
+
+    private fun setThemeRadio() {
         when (themeSharedPrefs.getTheme()) {
             0 -> binding.radioDayTheme.isChecked = true
             1 -> binding.radioNightTheme.isChecked = true
         }
     }
 
-    private fun setLanguageConfiguration() {
+    private fun setFontSizeRadio() {
+        when (fontSizeSharedPrefs.getFontSizeKey()) {
+            FontSize.SMALL -> binding.radioSizeSmall.isChecked = true
+            FontSize.MEDIUM -> binding.radioSizeMedium.isChecked = true
+            FontSize.BIG -> binding.radioSizeBig.isChecked = true
+        }
+    }
+
+    private fun setLanguageRadio() {
         when(languageSharedPrefs.getLanguage()) {
             Language.LANGUAGE_RUSSIAN -> binding.radioRussian.isChecked = true
             Language.LANGUAGE_ENGLISH -> binding.radioEnglish.isChecked = true
@@ -64,21 +77,38 @@ class SettingFragment : Fragment(R.layout.fragment_setting) {
     }
 
     private fun setClickListeners() {
+        setFontSizeRadioListener()
+        setThemeColorRadioListener()
+        setLanguageRadioListener()
+        setProfileListener()
+    }
+
+    private fun setFontSizeRadioListener() {
+        binding.textSizeRadioGroup.setOnCheckedChangeListener { _, id ->
+            when (id) {
+                binding.radioSizeSmall.id -> fontSizeSharedPrefs.setFontSize(FontSize.SMALL)
+                binding.radioSizeMedium.id -> fontSizeSharedPrefs.setFontSize(FontSize.MEDIUM)
+                binding.radioSizeBig.id -> fontSizeSharedPrefs.setFontSize(FontSize.BIG)
+            }
+        }
+    }
+
+    private fun setThemeColorRadioListener() {
         binding.colorThemeRadioGroup.setOnCheckedChangeListener { _, id ->
             when (id) {
                 binding.radioDayTheme.id -> {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
                     themeSharedPrefs.setTheme(0)
-                    return@setOnCheckedChangeListener
                 }
                 binding.radioNightTheme.id -> {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
                     themeSharedPrefs.setTheme(1)
-                    return@setOnCheckedChangeListener
                 }
             }
         }
+    }
 
+    private fun setLanguageRadioListener() {
         binding.languageRadioGroup.setOnCheckedChangeListener { _, id ->
             when (id) {
                 binding.radioRussian.id -> {
@@ -89,7 +119,9 @@ class SettingFragment : Fragment(R.layout.fragment_setting) {
                 }
             }
         }
+    }
 
+    private fun setProfileListener() {
         binding.profileLayout.setOnClickListener {
             if (UserAccount.uid.isEmpty()) {
                 logInUser()
