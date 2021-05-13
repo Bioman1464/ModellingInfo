d0914b2a9056e50dd1edd75ab6bc01dbec0031bd
Bioman1464 <danilgap1464@gmail.com>
2021-05-13 23:19:09 +0300

    feat(data.article): change article titles list names

--- app/src/main/java/bio/models/three_d/main_menu/common/article/ArticleData.kt
+++ app/src/main/java/bio/models/three_d/main_menu/common/article/ArticleData.kt
@@ -14,11 +14,11 @@ object ArticleData {
         if (articleTitles.size != 0) {
             articleTitles.clear()
         }
-        articleTitles.add(context.resources.getStringArray(R.array.blocking_article_titles))
-        articleTitles.add(context.resources.getStringArray(R.array.detaling_article_title))
-        articleTitles.add(context.resources.getStringArray(R.array.tech_article_titles))
-        articleTitles.add(context.resources.getStringArray(R.array.textures_article_titles))
-        articleTitles.add(context.resources.getStringArray(R.array.supply_article_titles))
+        articleTitles.add(context.resources.getStringArray(R.array.article_titles_1))
+        articleTitles.add(context.resources.getStringArray(R.array.article_titles_2))
+        articleTitles.add(context.resources.getStringArray(R.array.article_titles_3))
+        articleTitles.add(context.resources.getStringArray(R.array.article_titles_4))
+        articleTitles.add(context.resources.getStringArray(R.array.article_titles_5))
     }
 
     fun getList () : List<Article> {
@@ -30,7 +30,7 @@ object ArticleData {
 
     private fun createList (): List<Article> {
         val articleList = arrayListOf<Article>()
-        for (themeId in 0..4) {
+        for (themeId in 0..articleTitles.size) {
             val startId = getStartIdOfArticles(themeId)
             val endId = getEndIdOfArticles(themeId)
             val articleTitles = getArticleStringList(themeId)
@@ -41,12 +41,19 @@ object ArticleData {
                         themeId = themeId,
                         title = articleTitles[id - startId],
                         imageSrc = R.drawable.item_image,
-                        desc = articlesDescription?.get(themeId) ?: ""
+                        desc = getArticleDescription(id)
                     )
                 )
             }
         }
         return articleList
+    }
+
+    fun getArticleDescription(articleId: Int): String {
+        if (articleId in 0..articlesDescription.size) {
+            return articlesDescription[articleId] ?: ""
+        }
+        return ""
     }
 
     fun getByThemeId (themeId: Int): List<Article> {
@@ -63,27 +70,27 @@ object ArticleData {
     private fun getStartIdOfArticles (themeId: Int): Int {
         return when (themeId) {
             0 -> 0
-            1 -> 5
-            2 -> 9
-            3 -> 14
-            4 -> 16
+            1 -> 7
+            2 -> 11
+            3 -> 16
+            4 -> 22
             else -> 0
         }
     }
 
     private fun getEndIdOfArticles (themeId: Int): Int {
         return when (themeId) {
-            0 -> 4
-            1 -> 8
-            2 -> 13
-            3 -> 15
-            4 -> 21
-            else -> 4
+            0 -> 6
+            1 -> 10
+            2 -> 15
+            3 -> 21
+            4 -> 23
+            else -> 6
         }
     }
 
     private fun getArticleStringList (themeId: Int): Array<String> {
-        if (themeId in 0..5) {
+        if (themeId in 0..4) {
             return articleTitles[themeId]
         }
         return articleTitles[0]
